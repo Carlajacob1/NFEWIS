53b8a6af99ff13d3fd960ebac25376ac2f52373d
CarlJacob1 <jacobgwapo33@gmail.com>
2024-01-13 23:15:28 +0800

    lesson1

---
+++ lesson_01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    #this is a single line
+    echo "this comes after a single line comment";
+    /*Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+     Iusto quasi doloremque ipsum esse deleniti autem maiores quaerat 
+     reiciendis molestias nemo exercitationem maxime enim, alias aut a neque architecto, veritatis cumque?*/
+
+     echo "<br>this echo comes after a multiline comment";
+    ?>
+</body>
+</html>
